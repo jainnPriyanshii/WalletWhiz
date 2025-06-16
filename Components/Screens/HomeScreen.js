b1fff463a4cbc62638b6ab2ce15d6f5c7604996d
jainnpriyanshii <priyanshijain664@gmail.com>
2025-06-16 22:07:35 +0530

    Enhance Home and Statistics screens with UI components and charts; update package dependencies

--- Components/Screens/HomeScreen.js
+++ Components/Screens/HomeScreen.js
@@ -1,12 +1,165 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, FlatList, SafeAreaView } from 'react-native'
 import React from 'react'
+import {hp,wp} from '../../utils/Common'
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.nameContainer}>
+      <Text style={styles.helloText}>Hello,</Text>
+      <Text style={styles.nameText}>Priyanshi Jain</Text>
+      </View>
+      <View style={styles.balanceContainer}>
+        <View style={styles.balanceHeader}>
+    <Text style={styles.balanceLabel}>Total Balance</Text>
+    
+  </View>
+
+  <Text style={styles.balanceAmount}> ₹504.00</Text>
+       <View style={styles.incomeExpenseRow}>
+    <View style={styles.incomeBox}>
+      <Text style={styles.incomeArrow}>↓</Text>
+      <Text style={styles.incomeLabel}>Income</Text>
+      <Text style={styles.incomeAmount}>₹2429.00</Text>
     </View>
+
+    <View style={styles.expenseBox}>
+      <Text style={styles.expenseArrow}>↑</Text>
+      <Text style={styles.expenseLabel}>Expense</Text>
+      <Text style={styles.expenseAmount}>₹1925.00</Text>
+    </View>
+  </View>
+      </View>
+      <View>
+        <Text style={styles.transactionText}>Recent Transactions</Text>
+        <FlatList></FlatList>
+      </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: "#192019",
+    alignItems: "center",
+    // justifyContent:'center'
+  },
+  nameContainer:{
+    marginTop:hp(5),
+    marginLeft:wp(-50)
+    // alignItems:'flex-start',
+
+  },
+  helloText:{
+    color:'#e5e5e5',
+    fontSize:16,
+    textAlign:'left'
+  },
+
+  nameText:{
+    color:'#fff',
+    fontSize:24,
+    textAlign:'left'
+  },
+
+  balanceContainer:{
+    backgroundColor:'#e5e5e5',
+    height:hp(25),
+    width:wp(90),
+    borderRadius:25,
+    marginTop:hp(2)
+  },
+  
+  balanceHeader: {
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+ 
+},
+
+balanceLabel: {
+  fontSize: 20,
+  fontWeight: '600',
+  color: '#333',
+  //  marginLeft:wp(5),
+  marginTop:hp(1)
+
+},
+
+
+
+balanceAmount: {
+  fontSize: 28,
+  fontWeight: 'bold',
+  color: '#000',
+  // marginVertical: 10,
+  marginLeft:wp(30),
+  
+},
+
+incomeExpenseRow: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  marginTop: 10,
+  // marginLeft:wp(-10)
+},
+
+incomeBox: {
+  alignItems: 'center',
+  flex: 1,
+},
+
+expenseBox: {
+  alignItems: 'center',
+  flex: 1,
+},
+
+incomeArrow: {
+  color: 'green',
+  fontSize: 16,
+},
+
+expenseArrow: {
+  color: 'red',
+  fontSize: 16,
+},
+
+incomeLabel: {
+  color: '#333',
+  fontSize: 14,
+  marginTop: 4,
+},
+
+expenseLabel: {
+  color: '#333',
+  fontSize: 14,
+  marginTop: 4,
+},
+
+incomeAmount: {
+  color: 'green',
+  fontWeight: 'bold',
+  fontSize: 16,
+  marginTop: 2,
+  
+},
+
+expenseAmount: {
+  color: 'red',
+  fontWeight: 'bold',
+  fontSize: 16,
+  marginTop: 2,
+},
+ transactionText:{
+    fontSize: 20,
+    color: "#edede9",
+    marginTop:hp(2),
+    letterSpacing: wp(0.3),
+    fontWeight: "bold",
+    marginLeft:wp(-35)
+
+  }
+})
 
 export default HomeScreen
--- Components/Screens/StatisticsScreen.js
+++ Components/Screens/StatisticsScreen.js
@@ -1,12 +1,197 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, FlatList } from "react-native";
+import React, { useState } from "react";
+import { hp, wp } from "../../utils/Common";
+import {BarChart} from "react-native-gifted-charts";
 
 const StatisticsScreen = () => {
-  return (
-    <View>
-      <Text>StatisticsScreen</Text>
-    </View>
-  )
-}
+  const [selected, setSelected] = useState("Weekly");
+  const tabs = ["Weekly", "Monthly", "Yearly"];
 
-export default StatisticsScreen
+  const barData = [
+        {
+          value: 40,
+          label: 'Jan',
+          spacing: 2,
+          labelWidth: 30,
+          labelTextStyle: {color: 'gray'},
+          frontColor: '#177AD5',
+        },
+        {value: 20, frontColor: '#ED6665'},
+        {
+          value: 50,
+          label: 'Feb',
+          spacing: 2,
+          labelWidth: 30,
+          labelTextStyle: {color: 'gray'},
+          frontColor: '#177AD5',
+        },
+        {value: 40, frontColor: '#ED6665'},
+        {
+          value: 75,
+          label: 'Mar',
+          spacing: 2,
+          labelWidth: 30,
+          labelTextStyle: {color: 'gray'},
+          frontColor: '#177AD5',
+        },
+        {value: 25, frontColor: '#ED6665'},
+        {
+          value: 30,
+          label: 'Apr',
+          spacing: 2,
+          labelWidth: 30,
+          labelTextStyle: {color: 'gray'},
+          frontColor: '#177AD5',
+        },
+        {value: 20, frontColor: '#ED6665'},
+        {
+          value: 60,
+          label: 'May',
+          spacing: 2,
+          labelWidth: 30,
+          labelTextStyle: {color: 'gray'},
+          frontColor: '#177AD5',
+        },
+        {value: 40, frontColor: '#ED6665'},
+        {
+          value: 65,
+          label: 'Jun',
+          spacing: 2,
+          labelWidth: 30,
+          labelTextStyle: {color: 'gray'},
+          frontColor: '#177AD5',
+        },
+        {value: 30, frontColor: '#ED6665'},
+      ];
+
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.HeadText}>Statistics</Text>
+      <View style={styles.tabs}>
+        {tabs.map((tab) => (
+          <TouchableOpacity
+            key={tab}
+            onPress={() => setSelected(tab)}
+            style={[
+              styles.tabItem,
+              selected === tab && styles.activeTabItem,
+            ]}
+          >
+            <Text
+              style={[
+                styles.tabsText,
+                selected === tab && styles.activeTabText,
+              ]}
+            >
+              {tab}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+<View style={styles.chartContainer}>
+  {barData.length > 0 ? (
+      <BarChart data={barData}
+          barWidth={8}
+          spacing={24}
+          roundedTop
+          roundedBottom
+          hideRules
+          xAxisThickness={0}
+          yAxisThickness={0}
+          yAxisTextStyle={{color: 'gray'}}
+          noOfSections={3}
+          maxValue={75}
+  />) : (
+    <View style={styles.noChart}></View>
+  )}
+
+</View>
+<View style={styles.transactionConatiner}>
+  <Text style={styles.transactionText}>Transactions</Text>
+  <FlatList>
+
+  </FlatList>
+  </View>
+
+</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#192019",
+    alignItems: "center",
+  },
+
+  HeadText: {
+    fontSize: 24,
+    color: "#edede9",
+    marginTop: hp(5),
+    letterSpacing: wp(0.5),
+    fontWeight: "bold",
+  },
+
+  tabs: {
+    backgroundColor: "#353535",
+    width: wp(90),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: hp(5),
+    borderRadius: 25,
+    marginTop: hp(2),
+    padding: 4,
+  },
+
+  tabItem: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    borderRadius: 20,
+  },
+
+  activeTabItem: {
+    backgroundColor: "#edede9",
+  },
+
+  tabsText: {
+    color: "#edede9",
+    fontWeight: "bold",
+  },
+
+  activeTabText: {
+    color: "#000",
+  },
+
+  chartContainer:{
+position:'relative',
+marginTop:hp(3),
+// height:wp(25),
+justifyContent:'center',
+// alignItems:'center'
+  },
+
+  noChart:{
+  backgroundColor:'black',
+  heaight:hp(20)
+  },
+  transactionConatiner:{
+
+  },
+  transactionText:{
+    fontSize: 20,
+    color: "#edede9",
+    marginTop:hp(2),
+    letterSpacing: wp(0.3),
+    fontWeight: "bold",
+    marginLeft:wp(-42)
+
+  }
+});
+
+export default StatisticsScreen;
